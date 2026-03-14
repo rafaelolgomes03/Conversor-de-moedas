3a5923a14a3dd603e07273050141507996d9970b
rafaelolgomes03 <oliveiragomesrafael807@gmail.com>
2026-03-14 18:25:45 -0300

    Converção de moedas
    Converte um valor inserido em  real para dólar, euro e peso argentino.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿        Console.Write("Digite um valor em real para o programa efetuar as converções");
+                double real = double.Parse(Console.ReadLine());
+double dólar = real / 5.17;
+double euro = real / 6.14;
+        double p                     eso_argentino = real * 0.05;
+Console.WriteLine($"Valor em dólar: {dólar:F2}");
+Console.WriteLine($"Valor em euro: {euro:F2}");
+
+Console.WriteLine($"Valor em peso argentino: {peso_argentino:F2}");
